5c65b930d80f4baa2750fd170e562176ed9a25a7
shreypuranik <code@shreypuranik.co.uk>
2015-12-07 11:27:29 +0000

    Removing console.log

--- xpath-evaluator.js
+++ xpath-evaluator.js
@@ -3,7 +3,6 @@ $( document ).ready(function() {
     $("#check-xpath").click(function(){
         var siteURL = $("#site-url").val();
         var siteXpath = $("#xpath").val();
-        console.log("Site: "+siteURL+" Xpath: "+siteXpath);
         $("#evaluations").load("query.php?url="+siteURL+"&xpath="+siteXpath);
     });
 
