3f63fd3d599ad31477f1cd4a0a9e776b03bee572
shreypuranik <code@shreypuranik.co.uk>
2015-12-10 22:12:00 +0000

    Adding very basic styles and error message when nothing is submitted.

--- index.php
+++ index.php
@@ -3,8 +3,28 @@
     <title>Xpath Evaluator</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="xpath-evaluator.js"></script>
+    <style>
+        #mainBody{
+            width: 400px;
+            margin: auto;
+            font-family: Verdana;
+            font-size: 11px;
+        }
+        #response{
+            border-bottom: 1px dotted grey;
+            margin-bottom: 5px;
+        }
+        #check-xpath{
+            align:center;
+        }
+
+        #warning{
+            margin-top: 10px;
+            color: lightcoral;
+        }
+    </style>
 </head>
-<h1>Xpath Evaluator - check before you code!</h1>
+<h1 align="center">Xpath Evaluator - check before you code!</h1>
 <div id="mainBody">
 <p>As developers, before setting off to scrape a new website, it's useful to know if a simple xpath will work! Just feed in the URL of the page you want to scrape, and the xpath, and see the output. </p>
 <div class="form">
--- query.php
+++ query.php
@@ -1,12 +1,46 @@
 <?php
 
+if (empty($_GET['url'])
+    || empty($_GET['xpath']))
+{
+    echo '<div id="warning">Please ensure you have filled in both URL and xpath fields.</div>';
+    exit;
+}
+
 include_once("class.Scraper.php");
 
 $scraper = new Scraper();
 $scraper->setUpScraper($_GET['url'], $_GET['xpath']);
 $queryResponse = $scraper->getResponse();
-echo '<pre>';
-print_r($queryResponse);
-echo '</pre>';
+$output = "";
+$output .=<<<EOD
+<h3>Results of xpath query</h3>
+<p>Here's the result of the xpath query you ran. And as a bonus, the entire XML tree is <a href="#xml">below</a> too. </p>
+EOD;
+$output .= <<<EOD
+<div id="container">
+<div id="response">
+<pre>
+EOD;
+$output .= print_r($queryResponse, true);
+$output .= <<<EOD
+</pre>
+</div>
+<div id="xml">
+<h3>XML tree</h3>
+<p>If you need to refine your xpath, just copy the xml below and refactor your xpath.</p>
+EOD;
+$output .='<textarea  cols="100" rows="10">';
+$output .= print_r($scraper->getPageXML(), true);
+$output .='</textarea>';
+$output .=<<<EOD
+</div>
+<div style="clear:both;"></div>
+</div>
+
+EOD;
+
+
+echo $output;
 
 ?>
